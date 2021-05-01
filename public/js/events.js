a27e8889f70135313c2c0281d660e1543d7768ba
Glitch (periodic-delightful-yellowhorn) <none>
2021-05-01 13:34:40 +0000

    🐫🐮 Checkpoint ./public/js/events.js:6827812/395 ./views/home.pug:6827812/15689

--- public/js/events.js
+++ public/js/events.js
@@ -1,7 +1,9 @@
-const tabs = ['pairing','lobby','correspondence','inplay'];
+const tabs = {pairing:'lpools',lobby:'lreal_time',correspondence:'lseeks',inplay:''};
+const tcontent = [];
 $('#tabs').click(function (e) {
-  tabs.forEach(x=>document.getElementById(x).classList.remove('active'));
+  Object.keys(tabs).forEach(x=>document.getElementById(x).classList.remove('active'));
+  Object.keys(tabs).forEach(x=>document.getElementById(tabs[x]).style = "display:none");
   document.getElementById(e.target.id).classList.add('active');
-  
+  document.getElementById(tabs[e.target.id]).style = "display:inline";
 });
 
