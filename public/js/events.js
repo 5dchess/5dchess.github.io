e55083099cc414bea5a55d576e053e8349699340
Glitch (periodic-delightful-yellowhorn) <none>
2021-05-01 05:26:56 +0000

    🤑🍡 Checkpoint ./views/home.pug:6827812/31 ./public/js/events.js:6827812/349

---
+++ public/js/events.js
@@ -0,0 +1,6 @@
+$().on('click', '#submit', function(ev) {
+  if(this.status!="ongoing" || move.length==0) return;
+  console.log("submitting");
+  $("#submit")[0].disabled = true;
+  socket.emit('move',{player:playerColor,gameID:gameID, move:move});
+});
