bad62d02915ec7bab1c715e52c0c532a78696261
Glitch (periodic-delightful-yellowhorn) <none>
2021-05-01 11:48:41 +0000

    🌞🚘 Checkpoint ./package.json:6827812/585 ./public/js/events.js:6827812/537 ./views/home.pug:6827812/50

--- public/js/events.js
+++ public/js/events.js
@@ -1,15 +1,5 @@
-const tabs = [$('pairing'),$('lobby'),$('correspondence'),$('ingame')];
-$("#tabs").on('click', '#pairing', function(ev) {
-  console.log("uwu");
-  tabs.forEach(x=>x[0].class='');
-});
-$("#tabs").on('click', '#lobby', function(ev) {
-  tabs.forEach(x=>x[0].class='');
-});
-$("#tabs").on('click', '#correspondence', function(ev) {
-  tabs.forEach(x=>x[0].class='');
-});
-$("#tabs").on('click', '#ingame', function(ev) {
-  tabs.forEach(x=>x[0].class='');
-});
 console.log("uwu");
+const tabs = [$('pairing'),$('lobby'),$('correspondence'),$('ingame')];
+$('#tabs').click(function (e) {
+    alert(e.target.id); // The id of the clicked element
+});
