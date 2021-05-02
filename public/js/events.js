bb9c92529fa36c47e9cad9d4074146865c877351
Glitch (periodic-delightful-yellowhorn) <none>
2021-05-02 03:13:04 +0000

    🐏🐛 Checkpoint ./views/home.pug:6827812/62357 ./public/js/events.js:6827812/325

--- public/js/events.js
+++ public/js/events.js
@@ -1,10 +1,11 @@
-const tabs = {pairing:'lpools',lobby:'lreal_time',correspondence:'lseeks',inplay:'lnow_playing'};
-const tdisplay = {pairing:'grid',lobby:'inline',correspondence:'inline',inplay:'inline'}
+const tabs = {lobby:'lobbytab',spec:'spectab',ingame:'ingametab'};
+
+//switching between tabs
 $('#tabs').click(function (e) {
   if(!(e.target.id) in Object.keys(tabs)) return;
   Object.keys(tabs).forEach(x=>document.getElementById(x).classList.remove('active'));
   Object.keys(tabs).forEach(x=>document.getElementById(tabs[x]).style.display = "none");
   document.getElementById(e.target.id).classList.add('active');
-  document.getElementById(tabs[e.target.id]).style.display = tdisplay[e.target.id];
+  document.getElementById(tabs[e.target.id]).style.display = "inline";
 });
 
