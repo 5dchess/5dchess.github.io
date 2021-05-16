63829e36aa7b510ee1e087e5b71c0933162cd040
Glitch (periodic-delightful-yellowhorn) <none>
2021-05-16 23:56:00 +0000

    🐃👖 Checkpoint ./views/game.pug:6827812/16 ./routes/http.js:6827812/2151

--- routes/http.js
+++ routes/http.js
@@ -14,82 +14,26 @@ var home = function(req, res) {
  * Render "Game" Page (or redirect to home page if session is invalid)
  */
 var game = function(req, res) {
-
+  
   // Validate session data
   req.session.regenerate(function(err) {
+    console.log('owo'+req.SessionID);
     if (err) {res.redirect('/'); return; }
 
     // Find specified game
-    console.log("AAa+ "+window.location.href.substring("5dchess.glitch.me/game/".length));
-    var game = DB.find(window.location.href.substring("5dchess.glitch.me/game/".length));
+    let gameID = req.route.params.id;
+    var game = DB.find(gameID);
     if (!game) { res.redirect('/'); return;}
-
-    // Determine which player (color) to join as
-    var joinColor = (game.players[0].joined) ? game.players[1].color : game.players[0].color;
 
     // Save data to session
     req.session.gameID      = null;
-    req.session.playerColor = joinColor;
+    if(game.players['white']==req.SessionID){
+      req.session.playerColor;
+    }
   });
 
   // Render the game page
   res.render('game');
-};
-
-/**
- * Process "Start Game" form submission
- * Redirects to game page on success or home page on failure
- */
-var newGame = function(req, res) {
-
-  console.log(req.body);
-  // Create a new session
-  req.session.regenerate(function(err) {
-    if (err) { res.redirect('/'); return; }
-    
-    
-
-    // Create new game
-    
-
-    // Save data to session
-    req.session.gameID      = gameID;
-    req.session.playerColor = req.color;
-
-    // Redirect to game page
-    res.redirect('/game/'+gameID);
-  });
-};
-
-/**
- * Process "Join Game" form submission
- * Redirects to game page on success or home page on failure
- */
-var joinGame = function(req, res) {
-
-  // Create a new session
-  req.session.regenerate(function(err) {
-    if (err) {res.redirect('/'); return; }
-
-    // Validate form input
-    var validData = validateJoinGame(req);
-    if (!validData) { res.redirect('/'); return; }
-
-    // Find specified game
-    var game = DB.find(validData.gameID);
-    if (!game) { res.redirect('/'); return;}
-
-    // Determine which player (color) to join as
-    var joinColor = (game.players[0].joined) ? game.players[1].color : game.players[0].color;
-
-    // Save data to session
-    req.session.gameID      = validData.gameID;
-    req.session.playerColor = joinColor;
-    req.session.playerName  = validData.playerName;
-
-    // Redirect to game page
-    res.redirect('/game/'+validData.gameID);
-  });
 };
 
 /**
@@ -114,7 +58,6 @@ var quickMatch = function(req, res){
   //random color
   if(req.body.color=='random'){
     for(let key in DB.games){
-      console.log("K"+key);
       if(DB.games[key].players['white']==null){
         console.log("Joining game "+key);
         res.redirect('/game/'+key);
@@ -144,7 +87,8 @@ var quickMatch = function(req, res){
   //creates a game
   if(newgame){
     let newcolor = req.body.color=='random'?(Math.random()>0.5?'white':'black'):req.body.color;
-    var gameID = DB.add({player:req.SessionID, color:newcolor, views:req.body.views});
+    console.log('owo'+req.SessionID);
+    var gameID = DB.add({pID:req.SessionID, color:newcolor, views:req.body.views});
     console.log("Creating new game "+gameID);
     res.redirect('/game/'+gameID);
   }
